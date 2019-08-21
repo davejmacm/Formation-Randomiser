af1ecdbcd23565c1fb0b33ed6373a23c4c476e27
davejmacm <davejmacm@gmail.com>
2019-08-21 23:50:53 +0100

    added container and initial app files

---
+++ src/Container/FormationBox.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class FormationBox extends Component {
+    constructor(props){
+        super(props);
+    }
+
+
+render(){
+    return(
+        <h2>Formation</h2>
+    )
+  };
+
+
+};
+export default FormationBox;
