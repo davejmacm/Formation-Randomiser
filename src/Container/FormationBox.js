5ca8afbe2dd24087828c1367cef0e3cec47b6704
davejmacm <davejmacm@gmail.com>
2019-08-26 17:30:37 +0100

    added randomise func

--- src/Container/FormationBox.js
+++ src/Container/FormationBox.js
@@ -2,7 +2,26 @@ import React, {Component} from 'react';
 
 class FormationBox extends Component {
     constructor(props){
-        super(props);
+        super(props)
+        this.state = {
+            strikers:'',
+            forwards: '',
+            mids:'',
+            defs:''
+
+        }
+        this.randomise = this.randomise.bind(this)
+    }
+
+    randomise() {
+        var strikerNum = Math.floor(Math.random() * 11);
+        var forwardNum = Math.floor(Math.random() * (11-strikerNum));
+        var midNum = Math.floor(Math.random() * (11-strikerNum-forwardNum));
+        var defNum = (10-strikerNum-forwardNum-midNum);
+        this.setState({strikers: strikerNum})
+        this.setState({forwards: forwardNum})
+        this.setState({mids: midNum})
+        this.setState({defs: defNum})
     }
 
 
